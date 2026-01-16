dc27e81636277734a4bb3faabad894a0c032b819
Auguste Paccapelo <paccapelo.auguste@gmail.com>
2026-01-16 17:17:54 +0100

    [FIX] Refix OutIn ease

--- TweensProject/Assets/TweenCore/TweenScripts/TweenCorePropertyBase.cs
+++ TweensProject/Assets/TweenCore/TweenScripts/TweenCorePropertyBase.cs
@@ -312,13 +312,14 @@ public abstract class TweenCorePropertyBase
     {
         return t < 0.5f ?
             0.5f * TypeFunc(t * 2) :
-            1 - 0.5f * TypeFunc(2 - 2 * t);
+            1 - 0.5f * TypeFunc(2 * (1 - t));
     }
 
     private float OutIn(float t, Func<float, float> TypeFunc)
     {
-        return t > 0.5f ?
+        return t < 0.5f ?
             0.5f * (1 - TypeFunc(1 - t * 2)) :
-            0.5f + 0.5f * TypeFunc((t - 0.5f) * 2);
+            0.5f + 0.5f * TypeFunc((t - 0.5f) * 2f);
+            
     }
 }
